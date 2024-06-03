d260e65ecb320db9c3a8b7e422855d2f69302a41
Ethan Medrano <ethanmedrano52@icloud.com>
2024-06-02 20:08:58 -0400

    Adding additional functionality for returning list of transactions

--- modules/transaction.py
+++ modules/transaction.py
@@ -18,19 +18,23 @@ class Category(Enum):
 
 #Write a function that asks the user for transaction details (amount, category, date) and stores this data in a structured format.
 def get_new_transaction():
+    print("Enter transaction details: ")
     #Ask user for transaction amount
     amount = float(input('How much was it?'))
-    # Ask user for transaction category
-    category = input("Was it a want, need, or savings? ")
-
-    # Validate category input
-    while category not in ["want", "need", "savings"]:
-        print("Invalid category. Please enter 'want', 'need', or 'savings'.")
-        category = input("Was it a want, need, or savings? ")
-
     # Ask user for transaction date
     # Add functionality to check if its a valid number format
     date = input("What is today's date (e.g., YYYY-MM-DD)? ")
+
+    # Validate category input
+    while True: 
+        category_input = input("Was it a want, need, or savings?").lower()
+        try:
+            category = Category(category_input)
+            break
+        except ValueError:
+            print("Invalid Category. Please enter 'want', 'need' or 'savings'. ")
+
+
 
     #while date not :
     #    print("Invalid date format please ensure it is in YYYY-MM-DD")
@@ -39,14 +43,20 @@ def get_new_transaction():
     # Return a dictionary containing the transaction details
     return {
         "Amount": amount,
-        "Category": category,
+        "Category": category.value,
         "Date": date
     }
 
-transaction = get_new_transaction()
+transactions = []
 
-print(transaction)
+def add_transaction():
+    new_trans = get_new_transaction()
+    transactions.append(new_trans)
+    print("Transaction added successfully")
 
+#Example of invoking the function and storing it in a list for future use
+add_transaction()
+print(transactions)
 
 
 #print(transaction["Date"])
